2e55e3fb125baa4e40c5f9209d7c9e88a16ace7e
flaviostutz <flaviostutz@gmail.com>
2016-05-28 02:02:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+server.connection({port: 3000});
+
+server.route({
+    method: 'GET',
+    path: '/{yourname*}',
+    handler: function(req, reply) {
+        reply('Hello ' + req.params.yourname + '!')
+    }
+});
+
+server.start(function(){ // boots your server
+    console.log('Now Visit: http://localhost:3000/YOURNAME')
+});
+
+module.exports = server;
